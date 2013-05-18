6e5b2b5ce4e6802542deb48707d2c96a483822b4
Aoino <aoiro.aoino@gmail.com>
2013-05-18 13:09:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EeChat::Application.config.secret_token = '9f85c8634b0d63d2a5f96de1539316105aafd7e75be9ab77910b19dddff04f22adcd4b318ef480eff5b183ee44d74aa1f404e9352611d85d85d10ac8a46c4838'
