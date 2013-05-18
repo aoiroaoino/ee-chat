a6d7e42ad24e4071051680eaaa7c53d78433cc46
Aoino <aoiro.aoino@gmail.com>
2013-05-18 23:09:23 +0900

    twitter連携機能追加

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,4 +1,9 @@
+# -*- encoding: utf-8 -*-
 class RoomController < ApplicationController
   def index
+	  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	  	if @current_user.nil?
+	  		@current_user = User.new(:name => "ゲスト")
+	  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 EeChat::Application.routes.draw do
+	root :to => "room#index"
+	
   get "room/index"
+  get "sessions/destroy"
   get "socket/index", :as => :socket_index
   post "socket/index", :as => :socket_index
   post "/socket/create" => "socket#create", :as => :sc
+  
+  match "/auth/twitter/callback" => "sessions#create"
 end
