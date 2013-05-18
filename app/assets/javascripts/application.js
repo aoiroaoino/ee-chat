e4985185c753d5403c72287bd210633408925e93
Aoino <aoiro.aoino@gmail.com>
2013-05-18 15:47:23 +0900

    viewの修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,6 @@
 //= require jquery_ujs
 //= require_tree .
 
-var pusher = new Pusher('550cfffcc748b4bfe272');
-
 Pusher.log = function(message) {
       if (window.console && window.console.log) window.console.log(message);
     };
@@ -25,7 +23,7 @@ Pusher.log = function(message) {
 
 
     var pusher = new Pusher('550cfffcc748b4bfe272');
-    var channel = pusher.subscribe('test_channel');
-    channel.bind('my_event', function(data) {
+    var channel = pusher.subscribe('channel');
+    channel.bind('event', function(data) {
       alert(data);
     });
